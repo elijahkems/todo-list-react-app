ee9b69ff9f0066a8be93f8397f32a7b8a0e8baac
elijahkems <kemselijah@gmail.com>
2023-05-05 21:42:08 -0500

    final update

--- src/NewTodo.jsx
+++ src/NewTodo.jsx
@@ -15,7 +15,7 @@ export default function NewTodo({ toggleTodos, deleteTodos, todo }) {
                         hover:transition-all duration-[900ms]`;
   //
   return (
-    <li className=" relative m-2 my-4 px-4  pb-3 shadow-md shadow-primary-dark bg-primary-light rounded ">
+    <li className=" relative m-2 my-4 px-4  pb-3 shadow-md shadow-primary-dark bg-primary-light rounded hover:shadow-primary">
       <div className=" rounded-br-lg rounded-tl-lg relative p-1 top-0 left-[-1em] border-b-2 border-r-2 border-primary flex flex-row space-x-3 js max-w-fit ">
         <NewCheckbox
           className="rounded text-primary-darker"
